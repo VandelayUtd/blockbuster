fd246218d1a546f2265a8cb021bf07eb944af8b6
VandelayUtd <drewhadley.guitar@gmail.com>
2021-07-29 17:55:37 -0400

    built some reviews routes

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,23 +1,12 @@
 class MovieController < ApplicationController
 
 
-    get '/movies/:slug' do
-        if  logged_in? 
-            @movie = Movie.instock.find_by_slug(params[:slug])
-            @vhs = Movie.instock.select{|movie| movie.format == "VHS" && movie.title == @movie.title}.first
-            @dvd = Movie.instock.select{|movie| movie.format == "DVD" && movie.title == @movie.title}.first
-            # binding.pry
-            erb :"movies/new"
-
-        else 
-            redirect to "/login"
-        end
-    end
+    
 
     get '/movies' do  
         if logged_in?
         @user = current_user
-        @movies = @user.movies.select{|movie| movie.user_id != nil} 
+        @movies = Movie.list.select{|movie| movie.user_id == nil} 
         erb :"movies/index"
         else   
             redirect to "/login"
@@ -39,13 +28,25 @@ class MovieController < ApplicationController
         erb :"movies/show"
     end
   
-    patch '/movies' do
+    post '/movies' do
         if logged_in?
             @user = User.find_by(id: session[:user_id])
             movie_ids = params[:user][:movie_ids]
             movie_ids.each {|movie_id| @user.movies.push( Movie.find_by(id: movie_id))}
             redirect to "/users/#{@user.slug}"
         else
+            redirect to "/login"
+        end
+    end
+
+    get '/movies/:slug' do
+        if  logged_in? 
+            @movie = Movie.instock.find_by_slug(params[:slug])
+            @vhs = Movie.instock.select{|movie| movie.format == "VHS" && movie.title == @movie.title}.first
+            @dvd = Movie.instock.select{|movie| movie.format == "DVD" && movie.title == @movie.title}.first
+            # binding.pry
+            erb :"movies/new"
+        else 
             redirect to "/login"
         end
     end
--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,13 +1,28 @@
 class ReviewController < ApplicationController
+
     get '/reviews' do 
+        @user = current_user
+        erb :"reviews/index"
+    end
+
+
+    post '/reviews/:slug' do
+        binding.pry
+        user_review = current_user.reviews.build(comment: params[:comment], rating: params[:rating])
+        movie = Movie.find_by_slug(params[:slug])
+        movie.reviews << user_review
+        redirect to "/reviews"
+    end
+        
+    get "/reviews/:slug" do
+        @movie = Movie.find_by_slug(params[:slug])
+        @user = current_user
         erb :"reviews/show"
     end
 
-    get '/reviews/new' do 
+    
+    get '/reviews/:slug/new' do
+        @movie = Movie.find_by_slug(params[:slug])
         erb :"reviews/new"
-    end
-
-    post '/revews' do 
-        
     end
 end
