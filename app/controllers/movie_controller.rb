0b3d32d5fe10631eb68a7f041deddf4d2ae13692
VandelayUtd <drewhadley.guitar@gmail.com>
2021-08-02 17:08:57 -0400

    added logged_in? method to all routes

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -30,29 +30,41 @@ class MovieController < ApplicationController
     end
 
     get "/return" do
-        @user = current_user
-        erb :"movies/return"    
+        if logged_in?
+            @user = current_user
+            erb :"movies/return"
+        else 
+            redirect to "/login"
+        end
     end
 
-    get "/movies/:slug/reviews" do 
-        @movie = Movie.find_by_slug(params[:slug])
-        @user = current_user
-        erb :"movies/movie_reviews"
+    get "/movies/:slug/reviews" do
+        if logged_in?
+            @movie = Movie.find_by_slug(params[:slug])
+            @user = current_user
+            erb :"movies/movie_reviews"
+        else 
+            redirect to "/login"
+        end
     end
 
     patch '/return' do
-        @user = current_user
-        movie_ids = params[:user][:movie_ids]
-        
-        movie_ids.each do |id| 
-            movie = @user.movies.find_by(id: id)
-            if !movie.nil? 
-            movie.inventory += movie_ids.count{|i| i == movie.id.to_s}
-            movie.save
-            @user.movies.delete(movie)
+        if logged_in?
+            @user = current_user
+            movie_ids = params[:user][:movie_ids]
+            
+            movie_ids.each do |id| 
+                movie = @user.movies.find_by(id: id)
+                if !movie.nil? 
+                movie.inventory += movie_ids.count{|i| i == movie.id.to_s}
+                movie.save
+                @user.movies.delete(movie)
+                end
             end
+            erb :"movies/show"
+        else
+            redirect to "/login"
         end
-        erb :"movies/show"
     end
   
     
--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,42 +1,64 @@
 class ReviewController < ApplicationController
 
     get '/reviews' do 
-        @user = current_user         
-        erb :"reviews/index"
+        if logged_in?
+            @user = current_user         
+            erb :"reviews/index"
+        else 
+            redirect to "/login"
+        end
     end
 
     get '/reviews/new/:slug' do
-        @movie = Movie.find_by_slug(params[:slug])
-        erb :"reviews/new"
+        if logged_in? 
+            @movie = Movie.find_by_slug(params[:slug])
+            erb :"reviews/new"
+        else   
+            redirect to "/login"
+        end
     end
 
     post '/reviews/:slug' do
-        user_review = current_user.reviews.build(comment: params[:comment], rating: params[:rating])
-        movie = Movie.find_by_slug(params[:slug])
-        movie.reviews << user_review
-        redirect to "/movies/#{movie.slug}"
+        if logged_in?
+            user_review = current_user.reviews.build(comment: params[:comment], rating: params[:rating])
+            movie = Movie.find_by_slug(params[:slug])
+            movie.reviews << user_review
+            redirect to "/movies/#{movie.slug}"
+        else
+            redirect to "/login"
+        end
     end
 
     get '/reviews/:id/edit' do
-        @review = Review.find_by(id: params[:id])
-       
-        erb :"reviews/edit"
+        if logged_in?
+            @review = Review.find_by(id: params[:id])
+            erb :"reviews/edit"
+        else  
+            redirect to "/login"
+        end
     end
 
     patch '/reviews/:id' do
-        @review = Review.find_by(id: params[:id])
-        @review.comment = params[:comment]
-        @review.rating = params[:rating]
-        @review.save
-        redirect to "/movies/#{@review.movie.slug}"
+        if logged_in? 
+            @review = Review.find_by(id: params[:id])
+            @review.comment = params[:comment]
+            @review.rating = params[:rating]
+            @review.save
+            redirect to "/movies/#{@review.movie.slug}"
+        else 
+            redirect to "/login"
+        end
     end
 
-    delete '/reviews/:id/delete' do 
-        @review = Review.find_by(id: params[:id])
-        movie = @review.movie
-        @review.delete
-        redirect to "/movies/#{movie.slug}"
+    delete '/reviews/:id/delete' do
+        if logged_in? 
+            @review = Review.find_by(id: params[:id])
+            movie = @review.movie
+            @review.delete
+            redirect to "/movies/#{movie.slug}"
+        else 
+            redirect to "/login"
+        end
     end
-    
     
 end
